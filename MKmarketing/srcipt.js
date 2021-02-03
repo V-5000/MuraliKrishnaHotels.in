315463ca6a404e65e25291ae85feb63ad5d28214
V-5000 <77503114+V-5000@users.noreply.github.com>
2021-02-03 16:43:43 +0000

    Update srcipt.js

--- MKmarketing/srcipt.js
+++ MKmarketing/srcipt.js
@@ -1,38 +1,11 @@
-function openInNewTab(href) {
-  Object.assign(document.createElement('a'), {
-    target: '_blank',
-    href: href,
-  }).click();
-}
+var myImage = document.getElementById('mainImg_id');
+var imageArray = ["resorces/blue_bathroom.jpeg","resorces/blue_hall.jpeg","resorces/blue_bedroom.jpeg"];
+var imageIndex = 0;
 
-function call_now() {
-	openInNewTab('tel:+1234567890');
+function scrool() {
+	document.getElementById("mainImg_id").setAttribute("src",imageArray[imageIndex]);
+	imageIndex++;
+	if (imageIndex > imageArray.length-1) {
+		imageIndex = 0
+	}
 }
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
